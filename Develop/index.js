5a1bdd99f9bb260eeeb2524f70a8e302776ea6a6
Joshua Chu <jashya@gmail.com>
2020-07-08 20:36:19 -0400

    Add npm libraries, create basic api and file writing functionality

--- Develop/index.js
+++ Develop/index.js
@@ -1,11 +1,45 @@
-const questions = [
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const axios = require("axios");
 
+const writeFileAsync = util.promisify(fs.writeFile);
+
+const queryURL = "https://api.github.com/search/users?q=";
+
+const questions = [
 ];
 
 function writeToFile(fileName, data) {
 }
 
-function init() {
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type:"input",
+      name:"username",
+      message:"Provide your Github username to begin."
+    }
+  ]);
+}
+
+async function init() {
+  console.log("README Generator 2020");
+  try {
+    let input = await promptUser();
+
+    axios.get(queryURL+input.name).then(function(response) {
+      writeFileAsync("result.md",response);
+      console.log(response);
+    })
+
+    
+
+    console.log("Successfully wrote to result.md");
+    console.log(test);
+  } catch(err) {
+    console.log(err);
+  }
 
 }
 
